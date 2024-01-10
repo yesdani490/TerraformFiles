79b15247d17515a1d1a85ef4c50e6879fefaf03b
yesdani <yesdaniece@gmail.com>
2024-01-10 00:59:05 +0000

    new commit

--- count/count.tf
+++ count/count.tf
@@ -1,17 +1,17 @@
 resource "aws_instance" "countofinstances" {
     count = 10
     ami = var.myami
-    instance_type = var.instance-name[count.index] == "MongoDB" || var.instance-name[count.index] == "mysql" ? "t3.micro" : "t2.micro"
-    
+    instance_type = var.instance-names[count.index] == "MongoDB" || var.instance-names[count.index] == "mysql" ? "t3.micro" : "t2.micro"
+
     tags = {
-      name= var.instance-name[count.index]
+      name= var.instance-names[count.index]
     }
 }
 
 resource "aws_route53_record" "record" {
   count = 10
   zone_id = var.zone_id
-  name    = "${var.instance-name[count.index]}.joindevops.top"
+  name    = "${var.instance-names[count.index]}.joindevops.top"
   type    = "A"
   ttl     = 1
   records = [aws_instance.countofinstances[count.index].private_ip]
--- count/variable.tf
+++ count/variable.tf
@@ -3,7 +3,7 @@ variable "myami" {
   type = string
   default = "ami-03265a0778a880afb"
 }
-variable "instance-name" {
+variable "instance-names" {
     type = list
     default= ["MongoDB", "Cart", "shipping", "mysql", "rabbitmq", "web", "user", "catalogue", "redis", "payments" ]
   
