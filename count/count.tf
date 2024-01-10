78d92db41fe5ef0c875a58111daf2c719bb24dd6
yesdani <yesdaniece@gmail.com>
2024-01-10 00:29:05 +0000

    new commit

--- count/count.tf
+++ count/count.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "countofinstances" {
     count = 10
     ami = var.myami
-    instance_type = var.instance-name[count.index] == "MongoDB" ? "t3.micro" : "t2.micro"
+    instance_type = var.instance-name[count.index] == "MongoDB" || var.instance-name[count.index] == "mysql" ? "t3.micro" : "t2.micro"
     
     tags = {
       name= var.instance-name[count.index]
