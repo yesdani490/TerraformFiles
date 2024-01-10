4311f59ce694671b56aaefc5acb4c2140c5b1e93
yesdani <yesdaniece@gmail.com>
2024-01-10 00:07:44 +0000

    new commit

--- count/count.tf
+++ count/count.tf
@@ -1,9 +1,11 @@
 resource "aws_instance" "countofinstances" {
-
+    count = 10
     ami = var.myami
     instance_type = "t2.micro"
-    count = 10
+    
     tags = {
-      name= "Roboshop"
+      name= var.instance-name[count.index]
     }
 }
+
+# "MongoDB", "Cart", "shipping", "mysql", "rabbitmq", "web", "user", "catalogue", "redis", "payments"  
