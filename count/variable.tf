33c7c45040d5b7bec937c332e30ab1926d1bb688
yesdani <yesdaniece@gmail.com>
2024-01-10 00:06:41 +0000

    new commit

--- count/variable.tf
+++ count/variable.tf
@@ -3,3 +3,8 @@ variable "myami" {
   type = string
   default = "ami-03265a0778a880afb"
 }
+variable "instance-name" {
+    type = list
+    default= ["MongoDB", "Cart", "shipping", "mysql", "rabbitmq", "web", "user", "catalogue", "redis", "payments" ]
+  
+}
