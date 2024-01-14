246a879bee0598e092a1e8a2d1aeacc36d829389
yesdani <yesdaniece@gmail.com>
2024-01-14 22:00:52 +0000

    new commit

--- count/count.tf
+++ count/count.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "countofinstances" {
-    count = 10
+    count = 1
     ami = var.myami
     instance_type = var.instance_names[count.index] == "MongoDB" || var.instance_names[count.index] == "mysql" ? "t3.micro" : "t2.micro"
 
@@ -8,13 +8,17 @@ resource "aws_instance" "countofinstances" {
     }
 }
 
-resource "aws_route53_record" "record" {
-  count = 10
-  zone_id = var.zone_id
-  name    = "${var.instance_names[count.index]}.joindevops.top"
-  type    = "A"
-  ttl     = 1
-  records = [aws_instance.countofinstances[count.index].private_ip]
+resource "aws_key_pair" "deployer" {
+  key_name   = "teskeypair.pub"
+  public_key = file("${path.module}/testkeypair.pub")
 }
+# resource "aws_route53_record" "record" {
+#   count = 1
+#   zone_id = var.zone_id
+#   name    = "${var.instance_names[count.index]}.joindevops.top"
+#   type    = "A"
+#   ttl     = 1
+#   records = [aws_instance.countofinstances[count.index].private_ip]
+# }
 
 # "MongoDB", "Cart", "shipping", "mysql", "rabbitmq", "web", "user", "catalogue", "redis", "payments"  
